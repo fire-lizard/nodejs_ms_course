b39bc768e3fe491af2a47629196e276ad86b50f0
Anatoliy Sova <sovaar@hotmail.com>
2018-06-10 01:04:57 +1000

    lab 4

---
+++ lab4/server.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const logger = require('morgan')
+const errorhandler = require('errorhandler')
+const bodyparser = require('body-parser')
+let app = express()
+mongoose.Promise = global.Promise
+
+mongoose.connect('mongodb://localhost:27017/lab4db')
+
+app.use(logger('dev'))
+app.use(errorhandler())
+app.use(bodyparser.json())
+
+const accountSchema = mongoose.Schema({
+	name: String,
+	balance: Number
+})
+
+let Account = mongoose.model('Account', accountSchema)
+
+app.get('/accounts', (rweq, res) => {
+	Account.find({}, (err, accounts) => {
+		if (err)
+			res.send(err)
+		res.send(accounts)
+	})
+})
+
+app.post('/accounts', (req, res) => {
+	new Account({
+		name: req.body.name,
+		balance: req.body.balance
+	}).save((err) => {
+		if (err)
+			res.send(err)
+		res.send("Account Saved")
+	})
+})
+
+app.put('/accounts/:id', (req, res) => {
+	Account.findByIdAndUpdate({
+		'_id': req.params.id
+	}, {
+		name: req.body.name,
+		balance: req.body.balance
+	}, (err, acc) => {
+		if (err)
+			res.send(err)
+		res.send("Account Updated")
+	})
+})
+
+app.delete('/accounts/:id', (req, res) => {
+	Account.findByIdAndDelete({
+		'_id': req.params.id
+	}, (err, acc) => {
+		if (err)
+			res.send(err)
+		res.send("Account Deleted")
+	})
+})
+
+app.listen(3000)
